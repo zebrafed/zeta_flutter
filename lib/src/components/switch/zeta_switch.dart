652eb651cbd5810440184980aa9b7a0c1d529724
Atanas Yordanov <atanas.yordanov@athlonsofia.com>
2024-03-26 13:06:46 +0200

    remove size parameter and factory constructors

--- lib/src/components/switch/zeta_switch.dart
+++ lib/src/components/switch/zeta_switch.dart
@@ -18,22 +18,7 @@ class ZetaSwitch extends StatelessWidget {
     super.key,
     this.value = false,
     this.onChanged,
-    this.size,
   });
-
-  /// Constructor for [ZetaSwitch] for the platform Android.
-  const ZetaSwitch.android({
-    super.key,
-    this.value,
-    this.onChanged,
-  }) : size = _sizeAndroid;
-
-  /// Constructor for [ZetaSwitch] for the platform Android.
-  const ZetaSwitch.ios({
-    super.key,
-    this.value,
-    this.onChanged,
-  }) : size = _sizeIOS;
 
   /// Determines whether the switch is on or off.
   final bool? value;
@@ -41,29 +26,19 @@ class ZetaSwitch extends StatelessWidget {
   /// Called when the value of the switch should change.
   final ValueChanged<bool?>? onChanged;
 
-  /// Determines the size of the switch.
-  /// If not specified, the size will be determined
-  /// depending on the Platform and predefined constants.
-  final Size? size;
-
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties
       ..add(FlagProperty('value', value: value, ifTrue: 'on', ifFalse: 'off', showName: true))
-      ..add(ObjectFlagProperty<ValueChanged<bool>>('onChanged', onChanged, ifNull: 'disabled'))
-      ..add(DiagnosticsProperty<Size>('size', size));
+      ..add(ObjectFlagProperty<ValueChanged<bool>>('onChanged', onChanged, ifNull: 'disabled'));
   }
 
   @override
   Widget build(BuildContext context) {
     final zetaColors = Zeta.of(context).colors;
     return MaterialSwitch(
-      size: size == null
-          ? Platform.isIOS
-              ? _sizeIOS
-              : _sizeAndroid
-          : size!,
+      size: Platform.isIOS ? _sizeIOS : _sizeAndroid,
       trackOutlineWidth: const MaterialStatePropertyAll(0),
       trackOutlineColor: const MaterialStatePropertyAll(Colors.transparent),
       trackColor: MaterialStateProperty.resolveWith((states) {
