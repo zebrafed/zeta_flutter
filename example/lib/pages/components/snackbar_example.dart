be429b650c30df209ac7d2b79d7a2ba6fdaf9aaa
sd-athlon <simeon.athlon@gmail.com>
2024-03-27 09:01:50 +0200

    Add snackbar widgetbook

--- example/lib/pages/components/snackbar_example.dart
+++ example/lib/pages/components/snackbar_example.dart
@@ -80,230 +80,150 @@ class SnackBarExample extends StatelessWidget {
                     },
                   ),
                 ),
+
+                // Action
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Action",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.action,
+                          onPressed: () {},
+                          actionLabel: "Action",
+                          content: Text('Actionable message with icon'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Positive
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Positive",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.positive,
+                          content: Text('Request sent successfully'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Info
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Info",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.info,
+                          content: Text('Information is being displayed'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Info
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Info",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.info,
+                          content: Text('Information is being displayed'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Warning
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Warning",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.warning,
+                          content: Text('Warning has been issued'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Error
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Error",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.error,
+                          content: Text('Error has been detected'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Deletion
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "Deletion",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.deletion,
+                          onPressed: () {},
+                          content: Text('Item was deleted'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // View
+                Padding(
+                  padding: const EdgeInsets.only(top: ZetaSpacing.x4),
+                  child: ZetaButton.primary(
+                    label: "View",
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        ZetaSnackBar(
+                          context: context,
+                          type: ZetaSnackBarType.view,
+                          onPressed: () {},
+                          content: Text('Something neeeds your attention'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-enum ZetaSnackBarType {
-  defaultType,
-  action,
-  positive,
-  info,
-  warning,
-  error,
-  deletion,
-  view,
-}
-
-class ZetaSnackBar extends SnackBar {
-  ZetaSnackBar({
-    required BuildContext context,
-    required Widget content,
-    VoidCallback? onPressed,
-    ZetaSnackBarType? type,
-    Icon? leadingIcon,
-    bool rounded = true,
-    EdgeInsetsGeometry? margin,
-    String? actionLabel,
-    SnackBarBehavior? behavior = SnackBarBehavior.floating,
-  }) : super(
-          padding: EdgeInsets.zero,
-          backgroundColor: _getBackgroundColorForType(context, type),
-          margin: margin,
-          shape: RoundedRectangleBorder(
-            borderRadius: type != null
-                ? ZetaRadius.full
-                : rounded
-                    ? ZetaRadius.minimal
-                    : ZetaRadius.none,
-          ),
-          behavior: behavior,
-          content: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Row(
-                  children: [
-                    _LeadingIcon(type, leadingIcon),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: ZetaSpacing.s),
-                        child: _Content(type: type, child: content),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: ZetaSpacing.s),
-                child: _Action(
-                  type: type,
-                  actionLabel: actionLabel,
-                  onPressed: onPressed,
-                ),
-              )
-            ],
-          ),
-        );
-
-  static Color _getBackgroundColorForType(
-    BuildContext context,
-    ZetaSnackBarType? type,
-  ) {
-    final colors = Zeta.of(context).colors;
-
-    return switch (type) {
-      _ => colors.warm.shade100,
-    };
-  }
-}
-
-class _Content extends StatelessWidget {
-  const _Content({required this.child, required this.type});
-
-  final Widget child;
-  final ZetaSnackBarType? type;
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: ZetaTextStyles.bodyMedium,
-      child: child,
-    );
-  }
-}
-
-class _Action extends StatelessWidget {
-  const _Action({
-    required this.type,
-    required this.actionLabel,
-    required this.onPressed,
-  });
-
-  final ZetaSnackBarType? type;
-  final String? actionLabel;
-  final VoidCallback? onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    final colors = Zeta.of(context).colors;
-
-    return switch (type) {
-      ZetaSnackBarType.defaultType => _IconButton(
-          onPressed: () =>
-              ScaffoldMessenger.of(context).removeCurrentSnackBar(),
-          color: colors.iconInverse,
-        ),
-      ZetaSnackBarType.action => _ActionButton(
-          onPressed: () =>
-              ScaffoldMessenger.of(context).removeCurrentSnackBar(),
-          label: actionLabel,
-          color: colors.blue.shade50,
-        ),
-      ZetaSnackBarType.positive ||
-      ZetaSnackBarType.info ||
-      ZetaSnackBarType.warning ||
-      ZetaSnackBarType.error =>
-        _IconButton(
-          onPressed: () =>
-              ScaffoldMessenger.of(context).removeCurrentSnackBar(),
-          color: colors.cool.shade90,
-        ),
-      ZetaSnackBarType.deletion || ZetaSnackBarType.error => _IconButton(
-          onPressed: () =>
-              ScaffoldMessenger.of(context).removeCurrentSnackBar(),
-          color: colors.cool.shade90,
-        ),
-      _ => _ActionButton(
-          onPressed: onPressed,
-          label: actionLabel,
-          color: colors.blue.shade50,
-        ),
-    };
-  }
-}
-
-class _IconButton extends StatelessWidget {
-  const _IconButton({
-    required this.onPressed,
-    required this.color,
-  });
-
-  final VoidCallback? onPressed;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return IconTheme(
-      data: IconThemeData(
-        color: color,
-      ),
-      child: IconButton(
-        onPressed: onPressed,
-        icon: Icon(
-          ZetaIcons.close_round,
-        ),
-      ),
-    );
-  }
-}
-
-class _ActionButton extends StatelessWidget {
-  const _ActionButton({
-    required this.onPressed,
-    required this.label,
-    required this.color,
-  });
-
-  final VoidCallback? onPressed;
-  final String? label;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      padding: EdgeInsets.zero,
-      onPressed: onPressed,
-      child: Text(
-        label ?? '',
-        style: ZetaTextStyles.labelLarge.copyWith(color: color),
-      ),
-    );
-  }
-}
-
-class _LeadingIcon extends StatelessWidget {
-  const _LeadingIcon(this.type, this.icon);
-
-  final ZetaSnackBarType? type;
-  final Icon? icon;
-
-  Color _getIconColor(ZetaColors colors, ZetaSnackBarType? type) {
-    return switch (type) {
-      _ => colors.iconInverse,
-    };
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final colors = Zeta.of(context).colors;
-
-    return Padding(
-      padding: type != null || icon != null
-          ? const EdgeInsets.only(left: ZetaSpacing.s)
-          : EdgeInsets.zero,
-      child: IconTheme(
-        data: IconThemeData(
-          color: _getIconColor(colors, type),
-        ),
-        child: switch (type) {
-          _ => icon ?? SizedBox(),
-        },
       ),
     );
   }
---
+++ example/widgetbook/pages/components/snack_bar_widgetbook.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:zeta_flutter/zeta_flutter.dart';
+
+import '../../test/test_components.dart';
+
+Widget snackBarUseCase(BuildContext context) {
+  return WidgetbookTestWidget(
+    widget: Builder(
+      builder: (context) {
+        final text = context.knobs.string(
+          label: 'Content Text',
+          initialValue: 'This is a snackbar',
+        );
+
+        final actionLabel = context.knobs.stringOrNull(
+          label: 'Action Label',
+          initialValue: null,
+        );
+
+        final type = context.knobs.listOrNull<ZetaSnackBarType>(
+          label: 'Type',
+          options: [null, ...ZetaSnackBarType.values],
+          labelBuilder: (type) => type?.name ?? '',
+        );
+
+        final leadingIcon = context.knobs.listOrNull<Icon>(
+          label: 'Leading Icon',
+          options: [
+            null,
+            Icon(Icons.mood_rounded),
+            Icon(ZetaIcons.alarm_round),
+          ],
+          labelBuilder: (icon) => icon?.icon?.fontFamily ?? '',
+          initialOption: null,
+        );
+
+        final rounded =
+            context.knobs.boolean(label: 'Rounded', initialValue: true);
+
+        print(actionLabel);
+
+        return ZetaButton.primary(
+            label: "Show Snackbar",
+            onPressed: () {
+              print(actionLabel);
+              final snackBar = ZetaSnackBar(
+                context: context,
+                onPressed: () {},
+                actionLabel: actionLabel,
+                type: type,
+                leadingIcon: leadingIcon,
+                rounded: rounded,
+                content: Text(text),
+              );
+
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            });
+      },
+    ),
+  );
+}
